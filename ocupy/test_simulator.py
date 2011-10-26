22b2fadfadd2d4cda01460c202606bebd76f3696
Simon Harst <siharst@googlemail.com>
2011-10-26 15:55:01 +0200

    Fixed the test_anglendiff function, first working version.

--- ocupy/test_simulator.py
+++ ocupy/test_simulator.py
@@ -28,14 +28,14 @@ class TestSimulator(unittest.TestCase):
 		fm = fixmat.FixmatFactory('/home/student/s/sharst/Dropbox/NBP/fixmat_photos.mat')
 		gen = simulator.FixGen(fm)
 		
-		for angle in [-90, 0, 45, 90, 135]:
+		for angle in [-180, -90, 0, 45, 90, 135]:
 			print "Running anglendiff test on "+repr(angle)+" degrees."
 			coord = [(0,0)]
-	
 			length = 1
 			cur_angle = [np.nan]
 			cur_angle.append(angle)
 		
+			# Create artificial fixmat with fixed angle differences
 			for j in range(len(fm.x)-1):
 				coord.append(gen._calc_xy(coord[-1], cur_angle[-1], length))
 				cur_angle.append(cur_angle[-1]+angle)
@@ -43,34 +43,25 @@ class TestSimulator(unittest.TestCase):
 			fm.x = np.array([x[0] for x in coord])
 			fm.y = np.array([x[1] for x in coord])
 			
+			# Use anglendiff to calculate angles and angle_diff
 			a, l, ad, ld = simulator.anglendiff(fm, return_abs=True)
-			a = a[0]
+			a = np.round(a[0])	
+			a[a==-180]=180
 			
-			cur_angle = np.array(cur_angle)
+			cur_angle = simulator.reshift(np.array(cur_angle[0:-1]))
+			
 			# Assign nans to make arrays comparable
 			cur_angle[np.isnan(a)]=np.nan
-			cur_angle = simulator.reshift(cur_angle[0:-1]) # One element is added too much
-			cur_angle[cur_angle==-180]=180
-			#test = a==-180
-			
-			## Problem: Doesnt work!
-			
-			
-			#print a[test][0:20]
-			#a[test]=180
-			#print a[test][0:20]
-			a[a==-180]=180
-			angles = a
-			
-			ad[0] = np.round(simulator.reshift(ad[0][~np.isnan(ad[0])]))
+			cur_angle[np.round(cur_angle)==-180]=180
+				
+			ad = np.round(simulator.reshift(ad[0][~np.isnan(ad[0])]))
 			
 			if (angle==180 or angle==-180):
-				self.assertTrue(np.logical_or(ad[0]==angle, ad[0]==-angle).all())
+				self.assertTrue(np.logical_or(ad==angle, ad==-angle).all())
 				
 			else:
-				self.assertTrue((ad[0]==angle).all())
-				pdb.set_trace()
-				self.assertTrue((np.round(a[~np.isnan(a)])==simulator.reshift(cur_angle[~np.isnan(cur_angle)])).all())
+				self.assertTrue((ad==angle).all())
+				self.assertTrue((a[~np.isnan(a)]==cur_angle[~np.isnan(cur_angle)]).all())
 				
 
 	
