485b1b81224a4a2d7caa1a2287c9cd1358b5becc
Niklas Wilming <nwilming@uos.de>
2011-11-30 20:38:34 +0100

    First tests and first version of computing knot placement with marginal distributions

---
+++ ocupy/tests/test_splinebase.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import unittest
+
+import numpy as np
+
+from ocupy import spline_base as sb
+
+from scikits.learn import linear_model
+
+from pylab import *
+
+class Test1DSplines(unittest.TestCase):
+
+    def skip_cosine_fit(self):
+        x = np.cos(np.linspace(0,np.pi*2, 99))
+        noise = (np.random.random((99,))-.5)
+        plot(x+noise)
+        plot(x)
+        for num_splines in range(5,20,2):
+            splines = sb.spline_base1d(99, num_splines, spline_order = 5)
+            regression_models = [linear_model.Ridge(), linear_model.BayesianRidge()]
+            names = ['Ridge', 'BayesRidge']
+            for name, model in zip(names, regression_models):
+                model.fit(splines, x+noise)
+                y = model.predict(splines)
+                plot(y)
+                print num_splines, ' r^2 for ', name, ' is ', np.corrcoef(x+noise, y)[0,1]**2                     
+        show()
+        
+    def test_tangens(self):
+        x = np.tan(np.linspace(0,np.pi, 99))
+        noise = (np.random.random((99,))-.5)
+        plot(x+noise)
+        plot(x)
+        for num_splines in range(10,101,10):
+            splines = sb.spline_base1d(99, num_splines, spline_order = 5, marginal=x)
+            regression_models = [linear_model.Ridge(), linear_model.BayesianRidge()]
+            names = ['Ridge', 'BayesRidge']
+            for name, model in zip(names, regression_models):
+                model.fit(splines, x+noise)
+                y = model.predict(splines)
+                plot(y)
+                print num_splines, ' r^2 for ', name, ' is ', np.corrcoef(x+noise, y)[0,1]**2                     
+        show()
+    
+
+class Test2DSplines(unittest.TestCase):
+    
+    def skip_cosine_fit(self):
+        x = np.cos(np.linspace(0,np.pi*2, 99))
+        X = np.dot(x[:,newaxis],x[:,newaxis].T)
+        noise = (np.random.random(X.shape)-.5)*0
+        cnt = 1
+        subplot(4,5,cnt)
+        imshow(X+noise)
+        for num_splines in range(2,20,2):
+            splines = sb.spline_base(99,99, num_splines, spline_order = 3)
+            regression_models = [linear_model.Ridge(), linear_model.BayesianRidge()]
+            names = ['Ridge', 'BayesRidge']
+            for name, model in zip(names, regression_models):
+                model.fit(splines.T, (X+noise).flat)
+                y = model.predict(splines.T)
+                cnt += 1
+                subplot(4,5,cnt)
+                imshow(y.reshape(X.shape))
+                
+                print num_splines, ' r^2 for ', name, ' is ', np.corrcoef(X+noise, y.reshape(X.shape))[0,1]**2
+        show()
+    
+    def skip_non_continuous(self):
+        x = linspace(-10, 10, 99)
+        x = x**2
+        X = np.dot(x[:,newaxis],x[:,newaxis].T)
+        noise = (np.random.random(X.shape)-.5)
+        cnt = 1
+        subplot(4,5,cnt)
+        imshow(X+noise)
+        for num_splines in range(2,20,10):
+            splines = sb.spline_base(99,99, num_splines, spline_order = 3)
+            regression_models = [linear_model.Ridge(), linear_model.BayesianRidge()]
+            names = ['Ridge', 'BayesRidge']
+            for name, model in zip(names, regression_models):
+                model.fit(splines.T, (X+noise).flat)
+                y = model.predict(splines.T)
+                cnt += 1
+                subplot(4,5,cnt)
+                imshow(y.reshape(X.shape))
+
+                print num_splines, ' r^2 for ', name, ' is ', np.corrcoef(X+noise, y.reshape(X.shape))[0,1]**2
+        show()
+        
+if __name__ == '__main__':
+    unittest.main()
