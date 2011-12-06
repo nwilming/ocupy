c54347a51300a66031ac8dd3a434bac5e1d635ac
Niklas Wilming <nwilming@uos.de>
2011-12-06 15:47:55 +0100

    1D, 2D and 3D Spline fits + some first tests and comparisons to scipy kernel density estimates

--- ocupy/spline_base.py
+++ ocupy/spline_base.py
@@ -1,5 +1,6 @@
 import numpy as np
-import scikits.statsmodels.api as sm
+import scikits.statsmodels as sm
+from scikits.learn import linear_model
 
 def spline_fdm(fm,base_spec = None, spline_order = 3):
     # Set up parameters in relation to image_size
@@ -22,11 +23,12 @@ def spline_fdm(fm,base_spec = None, spline_order = 3):
     samples = np.array(zip((down_scale*fm.y), (down_scale*fm.x)))
     return spline_pdf(samples,e_y,e_x)
 
+#@deprecated
 def spline_pdf(samples,e_y,e_x,base=None,nr_knots_x=3,nr_knots_y=3,hist=None):
     height = len(e_y)-1
     width = len(e_x)-1
     if not base:
-        base = spline_base(height, width, nr_knots_x,nr_knots_y , 3)
+        base = spline_base2d(height, width, nr_knots_x,nr_knots_y , 3)
     if hist == None:
         (hist, _) = np.histogramdd(samples, (e_y, e_x))
     poiss_model = sm.Poisson(hist.reshape(-1,1), base.T)
@@ -74,24 +76,95 @@ def augknt(knots,order):
     [a.append(knots[-1]) for t in range(0,order)]
     return np.array(a)     
 
+def fit3d(samples, e_y, e_x, e_z, remove_zeros = False, **kw):
+    height, width, depth = len(e_y)-1, len(e_x)-1, len(e_z)-1 
+    
+    (p_est, _) = np.histogramdd(samples, (e_y, e_x, e_z))
+    p_est = p_est/sum(p_est.flat)
+    p_est = p_est.flatten()
+    if remove_zeros:
+        non_zero = ~(p_est == 0)
+    else:
+        non_zero = (p_est >= 0)
+    basis = spline_base3d(height, width, depth, **kw)
+    model = linear_model.BayesianRidge()
+    model.fit(basis[:, non_zero].T, p_est[:,np.newaxis][non_zero,:])
+    return model.predict(basis.T).reshape((height, width, depth)), p_est.reshape((height,width, depth))
+       
+       
+def fit2d(samples,e_y,e_x, remove_zeros = False, **kw):
+    height = len(e_y)-1
+    width = len(e_x)-1   
+    (p_est, _) = np.histogramdd(samples, (e_y, e_x))
+    p_est = p_est/sum(p_est.flat)
+    mx =  sum(p_est,1)
+    my = sum(p_est,0)
+    p_est = p_est.flatten()
+    if remove_zeros:
+        non_zero = ~(p_est == 0)
+    else:
+        non_zero = (p_est >= 0)
+    basis = spline_base2d(height, width, marginal_x = mx, marginal_y = my, **kw)
+    model = linear_model.BayesianRidge()
+    model.fit(basis[:, non_zero].T, p_est[:,np.newaxis][non_zero,:])
+    return model.predict(basis.T).reshape((height, width)), p_est.reshape((height,width))
+
+def fit1d(samples, e, bayesian_ridge = False):
+    samples = samples[~np.isnan(samples)]
+    length = len(e)-1
+    hist,_ = np.histogramdd(samples, (e,))
+    hist = hist/sum(hist)
+    basis = spline_base1d(length, nr_knots = 20, marginal = hist, spline_order = 5)
+    non_zero = hist>0
+    model = linear_model.BayesianRidge()
+    model.fit(basis[non_zero, :], hist[:,np.newaxis][non_zero,:])
+    return model.predict(basis), hist
+
+def find_fit_parameters2d(samples, e_x, e_y, max_params = 50):
+    x,y = np.mgrid[1:max_params, 1:max_params]
+    idx = (x**2+y**2)**.5 < max_params
+    x,y = x[idx], y[idx]    
+    results = []
+    for xp, yp in zip(x,y):
+        shuffle_idx = np.arange(0,len(samples[0]))
+        score = []
+        for r in range(10):
+            np.random.shuffle(shuffle_idx)
+            train_samples = (samples[0][shuffle_idx][::2], samples[1][shuffle_idx][0::2])
+            test_samples = (samples[0][shuffle_idx][1::2], samples[1][shuffle_idx][1::2])
+            fit, hist = fit2d(train_samples, e_y, e_x, nr_knots_x=xp, nr_knots_y=yp)
+            idx = [np.where((e_x > x) & (x >= e_x)) for x in samples[0]]
+            idy = [np.where((e_y > y) & (y >= e_y)) for y in samples[1]]
+            score += np.prod(fit[idx, idy])
+        results += [(xp, yp, np.mean(score))]
+    return results
+
+def knots_from_marginal(marginal, nr_knots, spline_order):
+    cumsum = np.cumsum(marginal)
+    cumsum = cumsum/cumsum.max()
+    borders = np.linspace(0,1,nr_knots)[1:]
+    intervals = [0] + [np.where(cumsum>=b)[0][0] for b in borders]
+    knot_placement = [t1+((t2-t1)/2) for t1,t2 in zip(intervals[:-1], intervals[1:])]
+    if not 0 in knot_placement:
+        knot_placement = [0] + knot_placement
+    if not len(marginal) in knot_placement:
+        knot_placement += [len(marginal)]
+    knots = augknt(knot_placement, spline_order)
+    return knots
+    
 def spline_base1d(length, nr_knots = 5, spline_order = 20, marginal = None):
     """Computes a 1D spline basis"""
     if marginal is None:
         knots = augknt(np.linspace(0,length, nr_knots), spline_order)
     else:
-        cumsum = np.cumsum(marginal)
-        cumsum = cumsum/cumsum.max()
-        borders = np.linspace(0,1,nr_knots)[1:]
-        intervals = [0] + [np.where(cumsum>=b)[0][0] for b in borders]
-        knot_placement = [t1+((t2-t1)/2) for t1,t2 in zip(intervals[:-1], intervals[1:])]
-        knots = augknt(knot_placement, spline_order)
+        knots = knots_from_marginal(marginal, nr_knots, spline_order)
         
     x_eval = np.arange(1,length+1).astype(float)
     Bsplines    = spcol(x_eval,knots,spline_order)
     return Bsplines
 
-def spline_base(height, width, Nr_Knots_x = 5.0, Nr_Knots_y = 5.0, 
-        spline_order = 3,scale_factor=None):
+def spline_base2d(height, width, nr_knots_x = 10.0, nr_knots_y = 10.0, 
+        spline_order = 3, marginal_x = None, marginal_y = None):
     """Computes a set of 2D spline basis functions. 
     
     The basis functions cover the entire space in height*width and can 
@@ -102,9 +175,9 @@ def spline_base(height, width, Nr_Knots_x = 5.0, Nr_Knots_y = 5.0,
             height of each basis
         width: int 
             widht of each basis
-        Nr_Knots_x: int
+        nr_Knots_x: int
             # of knots in x direction. These will be equally spaced
-        Nr_Knots_y: int
+        nr_Knots_y: int
             # of knots in y direction. These will be equally spaced
         spline_order: int
             Order of the spline.
@@ -112,29 +185,49 @@ def spline_base(height, width, Nr_Knots_x = 5.0, Nr_Knots_y = 5.0,
         bases: Matrix of size n*(width*height) that contains in each row
             one vectorized basis.
     """
-    Knots_x         = augknt(np.linspace(0,width,Nr_Knots_x),spline_order)
-    Knots_y         = augknt(np.linspace(0,height,Nr_Knots_y),spline_order)
-    if not scale_factor:
-        x_eval = np.arange(1,width+1).astype(float)
-        y_eval = np.arange(1,height+1).astype(float)
+    assert nr_knots_x<width and nr_knots_y<height, "Too many knots for size of the base"
+    if marginal_x is None:
+        knots_x         = augknt(np.linspace(0,width,nr_knots_x), spline_order)
     else:
-        x_eval = np.linspace(1,width, width*scale_factor)
-        y_eval = np.linspace(1,height, height*scale_factor)
-    spline_setx     = spcol(x_eval,Knots_x,spline_order)
-    spline_sety     = spcol(y_eval,Knots_y,spline_order)
-    Nr_coeff        = [spline_sety.shape[1], spline_setx.shape[1]]
-    DIM_Bspline     = [Nr_coeff[0]*Nr_coeff[1], len(x_eval)*len(y_eval)]
+        knots_x = knots_from_marginal(marginal_x, nr_knots_x, spline_order) 
+    if marginal_y is None:
+        knots_y         = augknt(np.linspace(0,height, nr_knots_y), spline_order)
+    else:
+        knots_y = knots_from_marginal(marginal_y, nr_knots_y, spline_order)
+    x_eval = np.arange(1,width+1).astype(float)
+    y_eval = np.arange(1,height+1).astype(float)    
+    spline_setx = spcol(x_eval, knots_x, spline_order)
+    spline_sety = spcol(y_eval, knots_y, spline_order)
+    nr_coeff = [spline_sety.shape[1], spline_setx.shape[1]]
+    DIM_Bspline = [nr_coeff[0]*nr_coeff[1], len(x_eval)*len(y_eval)]
     # construct 2D B-splines 
-    Nr_basis        = 0
-    Bspline         = np.zeros((DIM_Bspline[0],DIM_Bspline[1]))
-    for IDX1 in range(0,Nr_coeff[0]):
-        for IDX2 in range(0, Nr_coeff[1]):
-            Rand_coeff      = np.zeros((Nr_coeff[0],Nr_coeff[1]))
-            Rand_coeff[IDX1,IDX2] = 1
-            tmp = np.dot(spline_sety,Rand_coeff)
-            Bspline[Nr_basis,:]     = np.dot(tmp,spline_setx.T).reshape((1,-1))
-            Nr_basis                = Nr_basis+1
-    return Bspline
+    nr_basis = 0
+    bspline = np.zeros((DIM_Bspline[0],DIM_Bspline[1]))
+    for IDX1 in range(0,nr_coeff[0]):
+        for IDX2 in range(0, nr_coeff[1]):
+            rand_coeff  = np.zeros((nr_coeff[0] , nr_coeff[1]))
+            rand_coeff[IDX1,IDX2] = 1
+            tmp = np.dot(spline_sety,rand_coeff)
+            bspline[nr_basis,:] = np.dot(tmp,spline_setx.T).reshape((1,-1))
+            nr_basis = nr_basis+1
+    return bspline
+
+def spline_base3d(height, width, depth, nr_knots_x = 10.0, nr_knots_y = 10.0,
+        nr_knots_z=10, spline_order = 3, marginal_x = None, marginal_y = None):
+    """Computes a set of 3D spline basis functions. 
+    """    
+    basis2d = spline_base2d(height, width, nr_knots_x, nr_knots_y, spline_order, marginal_x, marginal_y)
+    knots_z = augknt(np.linspace(0,depth, nr_knots_z), spline_order)
+    z_eval = np.arange(1,depth+1).astype(float)
+    spline_setz = spcol(z_eval, knots_z, spline_order)
+    bspline = np.zeros((basis2d.shape[0]*len(z_eval), height*width*depth))
+    basis_nr = 0
+    for spline_a in spline_setz.T:
+        for spline_b in basis2d:
+            spline_b = spline_b.reshape((height, width))
+            bspline[basis_nr, :] = (spline_b[:,:,np.newaxis] * spline_a[:]).flat
+            basis_nr +=1
+    return bspline
 
 
 def N(u,i,p,knots):
--- ocupy/tests/test_splinebase.py
+++ ocupy/tests/test_splinebase.py
@@ -2,16 +2,18 @@
 # encoding: utf-8
 
 import os
+from os.path import join
 import unittest
 
 import numpy as np
+import scipy.ndimage as sndi
+from scipy.stats.kde import gaussian_kde
 
-from ocupy import spline_base as sb
+from ocupy import fixmat, simulator, spline_base as sb
 
 from scikits.learn import linear_model
-
+from ocupy import measures
 import h5py
-
 from pylab import *
 
 class Test1DSplines(unittest.TestCase):
@@ -51,7 +53,7 @@ class Test1DSplines(unittest.TestCase):
 
 class Test2DSplines(unittest.TestCase):
     
-    def test_cosine_fit(self):
+    def skip_cosine_fit(self):
         x = np.cos(np.linspace(0,np.pi*2, 99))
         X = np.dot(x[:,newaxis],x[:,newaxis].T)
         noise = (np.random.random(X.shape)-.5)*0
@@ -93,9 +95,193 @@ class Test2DSplines(unittest.TestCase):
 
                 print num_splines, ' r^2 for ', name, ' is ', np.corrcoef(X+noise, y.reshape(X.shape))[0,1]**2
         show()
+    
+    def test_compare_3d(self):
+        width, height, depth = 15,15,15
+        mat = 0.1*np.random.random((width, height, depth))
+        idx = range(0, np.prod(mat.shape))
+        mat[1:5,:,3] +=5
+        np.random.shuffle(idx)
+        idx = list(idx)
+        mat = mat.flatten()[idx].reshape((width, height, depth))
+        pdist = sndi.gaussian_filter(mat,3)
+        pdist += min(pdist.flatten())
+        pdist = pdist/sum(pdist)
+
+        cdist = cumsum(pdist.flat)
+        kl_fit = []
+        kl_kde = []
+        kl_h = []
+        xs = []
+        ion()
+        for num_samples in np.logspace(2,3.5,10):
+            xs += [num_samples]
+            print num_samples
+            samples = []
+            for x in range(int(num_samples)):
+                s = simulator.drawFrom(cdist)
+                x,y,z = np.unravel_index(s, pdist.shape)
+                samples += [[x,y,z]]
+            samples = np.array(samples)
+
+            e_y = e_x = linspace(0,width,width+1)
+            e_z = linspace(0,depth,depth+1)
+            fit, h = sb.fit3d(samples, e_y, e_x, e_z)
+            k = fit<0
+            fit[k] = 0   
+            fit = fit/sum(fit.flat)
+            h = h/sum(h.flat)
+            kde = gaussian_kde(samples.astype(float).T)
+            x, y, z = mgrid[0:width,0:height, 0:depth]
+            kde_fit = kde.evaluate((x.flatten(), y.flatten(), z.flatten())).reshape((width,height,depth))
+            kde_fit = kde_fit + min(kde_fit.flat)
+            kde_fit/=sum(kde_fit.flat)
+            kl_fit += [measures.kldiv(fit,pdist)]
+            kl_h += [measures.kldiv(h,pdist)]
+            kl_kde += [measures.kldiv(kde_fit,pdist)]
+
+
+            print 'O vs. F: ', measures.kldiv(pdist, fit)
+            print 'O vs. K: ', measures.kldiv(pdist, kde_fit)
+            print 'O vs. H:', measures.kldiv(pdist, h)
+            kde_fit = kde_fit.sum(2)
+            fit = fit.sum(2)
+            h = h.sum(2)
+            pdist2 = pdist.sum(2)
+            mx = np.max([np.max(h), np.max(kde_fit.flatten()), np.max(fit.flatten()), np.max(pdist2.flatten())])
+            mi = np.min([np.min(h), np.min(kde_fit.flatten()), np.min(fit.flatten()), np.min(pdist2.flatten())])
+            subplot(1,5,1)
+            plot(xs,kl_fit)
+            #plot(xs,kl_h)
+            plot(xs,kl_kde)
+            subplot(1,5,2)
+            imshow(fit)
+            clim([mi,mx])
+            subplot(1,5,3)
+            imshow(kde_fit)
+            clim([mi,mx])
+            subplot(1,5,4)            
+            imshow(h)
+            clim([mi,mx])
+            subplot(1,5,5)
+            imshow(pdist2)
+            clim([mi,mx])
+
+            waitforbuttonpress()
+        legend(['Spline fit', 'KDE fit'])
+        
+        show()
+        
+         
+            
+    def skip_compare_2d(self):
+        mat = 2*np.eye(100) +  np.random.random((100,100))
+        np.random.shuffle(mat)
+        pdist = sndi.gaussian_filter(mat,5)
+        pdist = pdist/sum(pdist)
+        cdist = cumsum(pdist.flat)
+        kl_fit = []
+        kl_kde = []
+        kl_h = []
+        xs = []
+        ion()
+        for num_samples in np.logspace(2,3.5,10):
+            xs += [num_samples]
+            print num_samples
+            samples = []
+            for x in range(int(num_samples)):
+                s = simulator.drawFrom(cdist)
+                x,y = np.unravel_index(s, pdist.shape)
+                samples += [[x,y]]
+            samples = np.array(samples)
+            
+            e_y = e_x = linspace(0,100,101)
+            fit, h = sb.fit2d(samples, e_y, e_x)
+            k = fit<0
+            fit[k] = 0   
+            fit = fit/sum(fit.flat)
+            
+            kde = gaussian_kde(samples.astype(float).T)
+            x, y = mgrid[0:100,0:100]
+            kde_fit = kde.evaluate((x.flatten(), y.flatten())).reshape((100,100))
+            kde_fit = kde_fit + min(kde_fit.flat)
+            kde_fit/=sum(kde_fit.flat)
+            kl_fit += [measures.kldiv(fit,pdist)]
+            kl_h += [measures.kldiv(h,pdist)]
+            kl_kde += [measures.kldiv(kde_fit,pdist)]
+            print 'O vs. F: ', measures.kldiv(pdist, fit)
+            print 'O vs. K: ', measures.kldiv(pdist, kde_fit)
+            print 'O vs. H:', measures.kldiv(pdist, h)
+            subplot(1,5,1)
+            plot(xs,kl_fit)
+            plot(xs,kl_h)
+            plot(xs,kl_kde)
+            subplot(1,5,2)
+            imshow(fit)
+            subplot(1,5,3)
+            imshow(kde_fit)
+            subplot(1,5,4)
+            imshow(h)
+            subplot(1,5,5)
+            imshow(pdist)
+            waitforbuttonpress()
+        legend(['Spline fit', 'KDE fit', 'Histogram'])
+        
+        show()
+        
+            
 
 class TestFixationFits(unittest.TestCase):
+    
+    def skip_fixmats_1D(self):
+        path = '/Users/nwilming/ior/data'
+        
+        fixmats = [fixmat.FixmatFactory(join(path,fname)) for fname in ['afc_patch_recognition_fixmat.mat',
+                    'age_study_fixmat.mat',
+                    'patch_recognition_fixmat.mat']]
+        for fm in fixmats:
+            data = simulator.anglendiff(fm, return_abs=True)
+            for i,(datum, name, e) in enumerate(zip(data,
+                                            ['Angles', 'Length', 'DAngles', 'DLengths'],
+                                            [linspace(0,180,90), linspace(0,700,100),
+                                             linspace(-180,180,90),linspace(-700,700,100)])):
+                subplot(2,2,i+1)  
+                p, h = sb.fit1d(datum[0][0::2], e)
+                p2, h2 = sb.fit1d(datum[0][1::2], e)
+                plot(p)  
+                plot(h2)
+                title(name)
+        show()
 
+    def skip_fixmats_2D(self):
+        path = '/Users/nwilming/ior/data'
+
+        fixmats = [fixmat.FixmatFactory(join(path,fname)) for fname in ['afc_patch_recognition_fixmat.mat',
+                    'age_study_fixmat.mat',
+                    'patch_recognition_fixmat.mat']]
+        cnt = 1
+        for fmi, fm in enumerate(fixmats):
+            data = simulator.anglendiff(fm, return_abs=True)
+            data = [(data[0], data[1]),(data[2], data[3])]
+            for i,(datum, name, e) in enumerate(zip(data,
+                                            ['Angles and Lengths', 'Delta Angles and Lengths'],
+                                            [(linspace(0,180,90), linspace(0,700,100)),
+                                             (linspace(-180,180,90),linspace(-700,700,100))])):
+
+                subplot(3,4,cnt)
+                cnt+=1
+                p, h = sb.fit2d((datum[1][0][0::2],datum[0][0][0::2]) , e[1], e[0])
+                p2, h2 = sb.fit2d((datum[1][0][1::2],datum[0][0][1::2]) , e[1], e[0])
+                imshow(p)  
+                title(name)
+                subplot(3,4,cnt)
+                cnt+=1
+                imshow(h2)
+                title(name)
+        show()            
+    
+
+    
     def skip_err_samples(self):
         # For now hard code path
         data = h5py.File('/home/student/n/nwilming/Desktop/spline_fit_example.hdf5')
